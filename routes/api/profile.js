16c6cb3df479dbe828350ad799fb17ed61422393
sheikh-bilal <reply2bilal.ahmed@gmail.com>
2021-04-21 06:03:41 -0700

    Profile: Get all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -95,5 +95,18 @@ async (req, res) => {
 
 });
 
+// @route       GET api/profile
+// @desc        GET all user profiles
+// @access      Public
+
+router.get('/', async(req,res)=>{
+  try {
+    const profiles = await Profile.find().populate('user',['name', 'avatar']);
+    res.json(profiles);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router; 
