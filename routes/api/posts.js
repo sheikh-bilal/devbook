1c07f159bb4f4f614b7f88a47a6f8302c7ba6817
sheikh-bilal <reply2bilal.ahmed@gmail.com>
2021-04-22 03:43:22 -0700

    Post: GET post by Id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -49,4 +49,25 @@ router.get('/', auth, async(req,res)=>{
     res.status(500).send('Server Error!');
   }
 });
+
+// @route       GET api/posts/:id
+// @desc        GET post by id
+// @access      Private
+router.get('/:id', auth, async(req,res)=>{
+  try {
+    const post = await Post.findById(req.params.id);
+    if(!post){
+      return res.status(404).json({ msg: 'Post not found!'});
+    }
+    res.json(post);
+
+  } catch (error) {
+    console.error(error.message);
+    if(error.kind === 'ObjectId'){
+      return res.status(404).json({ msg: 'Post not found!'});
+    }
+    res.status(500).send('Server Error!');
+  }
+});
+
 module.exports = router; 
