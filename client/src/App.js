adeb35cca1becb23bd71c7a55988d99bc3083520
sheikh-bilal <reply2bilal.ahmed@gmail.com>
2021-04-27 02:32:40 -0700

    Implement Login reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
-import {Fragment} from 'react';
+import {Fragment, useEffect} from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-import Alert from './components/layout/Alert'; 
+import Alert from './components/layout/Alert';
+
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
-const App = () => (
+if(localStorage.token){
+  setAuthToken(localStorage.token);
+}
+
+const App = () => { 
+  useEffect(()=> {
+    store.dispatch(loadUser());
+  }, []);
+
+  return(
   <Provider store={store}>
     <Router>
       <Fragment>
@@ -26,6 +38,6 @@ const App = () => (
       </Fragment>
     </Router>
   </Provider>
-);
+)};
 
 export default App;
