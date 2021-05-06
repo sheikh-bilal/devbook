23947210f659ac499ff668cd24eb5f293fb7199e
sheikh-bilal <reply2bilal.ahmed@gmail.com>
2021-05-06 04:33:14 -0700

    Implement Add experience & education component

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 import CreateProfile from './components/profile-form/CreateProfile';
 import EditProfile from './components/profile-form/EditProfile';
+import AddExperience from './components/profile-form/AddExperience';
+import AddEducation from './components/profile-form/AddEducation';
 
 
 // Redux
@@ -41,6 +43,8 @@ const App = () => {
             <PrivateRoute exact path='/dashboard' component={Dashboard}/>
             <PrivateRoute exact path='/create-profile' component={CreateProfile}/>
             <PrivateRoute exact path='/edit-profile' component={EditProfile}/>
+            <PrivateRoute exact path='/add-experience' component={AddExperience}/>
+            <PrivateRoute exact path='/add-education' component={AddEducation}/>
           </Switch>
         </section>
       </Fragment>
